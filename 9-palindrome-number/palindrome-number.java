6cb22a47cd6dee525a4752306b7d4904d8833448
Pranab Prakash Das <pranabpd02@gmail.com>
2026-04-10 01:32:15 +0530

    Time: 5 ms (79.49%) | Memory: 46 MB (61.05%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }   
+
+        int temp = x, rev = 0;
+        while(x > 0){
+            int dig = x % 10;
+            rev = rev*10 + dig;
+            x = x / 10;
+        }
+
+        if(temp == rev){
+            return true;
+        }
+        return false;
+    }
+}
